96bdff7edf09f15075e6ef84600534d5a7a0729f
burke <31494378+burkemtaylor@users.noreply.github.com>
2024-04-23 00:31:32 -0400

    feat: setup home page

---
+++ src/constants/headerData.ts
@@ -0,0 +1,17 @@
+import { HeaderProps } from "~/interfaces/header";
+
+export const headerData: HeaderProps = {
+  title: "burke taylor",
+  menuItems: [
+    { name: "resume", link: "/resume" },
+    {
+      name: "projects",
+      link: "/projects",
+      children: [
+        { name: "project1", link: "/project1" },
+        { name: "project2", link: "/project2" },
+      ],
+    },
+    { name: "contact", link: "/contact" },
+  ],
+};
