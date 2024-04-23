cdc7d6920df1666c050eeaf464dd8978e7ead374
burke <31494378+burkemtaylor@users.noreply.github.com>
2024-04-23 11:24:35 -0400

    feat: setup resume page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="business">
       <body
         className={`font-sans ${inter.variable} flex min-h-screen flex-col`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center text-white">
-      <div
-        className="hero flex-1"
-        style={{
-          backgroundImage:
-            "url(https://utfs.io/f/cd44ea80-8375-4a67-a9cc-549eaf23e16d-1xdpe9.jpeg)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hi, I'm Burke</h1>
-            <p className="mb-5">
-              I've been building & maintaining web apps for 5 years.
-            </p>
-            <button className="btn btn-primary">Get to know me</button>
-          </div>
+    <div
+      className="hero flex-1"
+      style={{
+        backgroundImage:
+          "url(https://utfs.io/f/cd44ea80-8375-4a67-a9cc-549eaf23e16d-1xdpe9.jpeg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hi, I'm Burke</h1>
+          <p className="mb-5">
+            I've been building & maintaining web apps for 5 years.
+          </p>
+          <button className="btn btn-primary">Get to know me</button>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
