a27063809f7bdf4b954c272e5c8ebda3283944f4
burke <31494378+burkemtaylor@users.noreply.github.com>
2025-01-17 18:20:57 -0500

    fix: chrome scroll
    Chrome doesn't obscure items below the edge of the window on iOS. Extend footer to ensure this behavior.

--- src/_utilities/nodeWrapper.tsx
+++ src/_utilities/nodeWrapper.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+
 export function nodeWrapper(
   node: React.ReactNode | React.ReactNode[],
 ): React.ReactNode {
   if (Array.isArray(node)) {
-    return <>{node.map((child) => child)}</>;
+    return (
+      <>
+        {node.map((child, i) => (
+          <React.Fragment key={i}>{child}</React.Fragment>
+        ))}
+      </>
+    );
   } else {
     return node;
   }
--- src/app/about/loading.tsx
+++ src/app/about/loading.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
+import Card from "~/_components/_primitives/card";
+
 export default function Loading() {
   return (
-    <main className="flex h-full w-full flex-1 flex-col items-center gap-8 overflow-y-auto p-8"></main>
+    <div className="flex h-full w-full flex-1 flex-col items-center gap-8 overflow-y-auto mobile:overflow-y-scroll mobile:px-8 mobile:pb-24 mobile:pt-24 desktop:p-8"></div>
   );
 }
